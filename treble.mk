743d4cf8e86a6b23157e2e6704465f36c008219b
Christian Oder <myself5@carbonrom.org>
2017-11-15 16:23:22 +0100

    shamu: build AOSP nfc hal
    our NFC hal has the required changes for brcm chips no need to use a seperate hal
    
    Change-Id: I05793bc587aebaaafb52e4bbf933ec444f236fec

--- treble.mk
+++ treble.mk
@@ -55,7 +55,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl-bcm \
+    android.hardware.nfc@1.0-impl \
     android.hardware.nfc@1.0-service
 
 # RenderScript
