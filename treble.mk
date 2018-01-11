c8274c48e4d862afc45eeb07845028fe68181ac4
elektroschmock <elektroschmock78@googlemail.com>
2018-01-11 09:39:09 +0100

    shamu: Remove GNSS service
    * Our HAL is not compatible enough
    
    Change-Id: I37a2e753a42d9de3ee3b187dceba71ce36907742

--- treble.mk
+++ treble.mk
@@ -38,8 +38,7 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service
+    android.hardware.gnss@1.0-impl
 
 # Graphics
 PRODUCT_PACKAGES += \
