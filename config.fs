09f646ad1d74ce1e5912a0b8af7c114cd6d5c173
Christian Oder <myself5@carbonrom.org>
2019-09-12 20:59:22 +0200

    shamu: WIP
    Change-Id: Ie8af782dd94ea5363fe76d3313921faf4c131efb

--- config.fs
+++ config.fs
@@ -9,3 +9,15 @@ mode: 0700
 user: AID_CAMERA
 group: AID_SHELL
 caps: SYS_NICE
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
--- device.mk
+++ device.mk
@@ -64,10 +64,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/BCM4356A2_001.003.015.0077.0214_ORC.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/bcm4354A2.hcd
 
-# Camera
-PRODUCT_PACKAGES += \
-    Snap
-
 PRODUCT_PACKAGES += \
     camera.msm8084 \
     libmm-qcamera
