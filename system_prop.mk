93cbdaf79d84d530e2fbd957d9782278e20fdf5a
Christian Oder <myself5@carbonrom.org>
2021-07-26 01:17:14 +0200

    Revert "shamu: Set heapminfree to 512k"
    This reverts commit 821748bc5ee4d4bda3e4a82b82b42ec10e3e4702.
    
    Change-Id: I48f09cab1f5fc7eea63a262071c3145cf3f5e184

--- system_prop.mk
+++ system_prop.mk
@@ -48,7 +48,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
 
 
